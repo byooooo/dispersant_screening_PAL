4232230336adacb29ee3821ae134283f5a1a50db
Kevin <kevin.jablonka@epfl.ch>
2020-05-08 14:00:11 +0200

    feat: had a quick look at the data, merged data.
    Former-commit-id: aba7abe6f6939e9511f46e2dc6417f697a573a54

---
+++ dispersant_screener/definitions.py
@@ -0,0 +1,24 @@
+TARGETS = [
+    'deltaGmin',
+    'A2_normalized'
+]
+
+FEATURES = ['[W]',
+ '[Ta]',
+ '[Tr]',
+ '[R]',
+ 'total_count',
+ 'R_avg_cluster_size',
+ 'Ta_avg_cluster_size',
+ 'Tr_avg_cluster_size',
+ 'W_avg_cluster_size',
+ 'num_cluster_types',
+ 'max_cluster_size',
+ 'R_no_of_cluster',
+ 'Ta_no_of_cluster',
+ 'Tr_no_of_cluster',
+ 'W_no_of_cluster',
+ 'R_max_cluster_size',
+ 'Ta_max_cluster_size',
+ 'Tr_max_cluster_size',
+ 'W_max_cluster_size']
