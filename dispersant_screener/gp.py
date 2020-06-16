54d8f98719d05f89ec53d72b61508409dd663f1b
Kevin <kevin.jablonka@epfl.ch>
2020-06-16 09:45:58 +0200

    chore: trying to trigger CI again
    Former-commit-id: 96ae3d674f89afce08d5d41f3d357ee106f73edf [formerly 5d0f30de94c48346bd4f9395605aec20c586a955]
    Former-commit-id: 4f6febf9bf37dc8434f279db252ec0f588962892

--- dispersant_screener/gp.py
+++ dispersant_screener/gp.py
@@ -2,3 +2,19 @@
 """
 Implementing the Gaussian Process Regression models.
 """
+from __future__ import absolute_import
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+
+def build_virial_model():
+    virial_model = GaussianProcessRegressor(kernel=RBF(), normalize_y=True, n_restarts_optimizer=30)
+
+    return virial_model
+
+
+def build_gibbs_model():
+    gibbs_model = GaussianProcessRegressor(kernel=RBF(), normalize_y=True, n_restarts_optimizer=30)
+
+    return gibbs_model
--- tests/test_pal.py
+++ tests/test_pal.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 
-from dispersant_screener.pal import _get_gp_predictions
+from dispersant_screener.pal import (_get_gp_predictions, _get_uncertainity_region)
 
 
 def test__get_gp_predictions(make_random_dataset):
@@ -27,3 +27,24 @@ def test__get_gp_predictions(make_random_dataset):
 
     assert mu.shape == y_input.shape
     assert std.shape == y_input.shape
+
+
+def test__get_uncertainity_region():
+    """make sure that the uncertainity windows is computed in a reasonable way"""
+    mu = 1 # pylint:disable=invalid-name
+
+    low0, high0 = _get_uncertainity_region(mu, 0, 1)
+    assert low0 == mu
+    assert high0 == mu
+
+    low1, high1 = _get_uncertainity_region(mu, 1, 0)
+    assert low1 == mu
+    assert high1 == mu
+
+    low2, high2 = _get_uncertainity_region(mu, 0, 0)
+    assert low2 == mu
+    assert high2 == mu
+
+    low3, high3 = _get_uncertainity_region(mu, 1, 1)
+    assert low3 == 0
+    assert high3 == 2
