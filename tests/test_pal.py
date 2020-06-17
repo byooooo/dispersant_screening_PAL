350b7b8db34c2daf462ae25bfec8b2aa38788d9c
Kevin <kevin.jablonka@epfl.ch>
2020-06-17 11:29:38 +0200

    feat: starting implementing tests for PAL
    Former-commit-id: af44db05812fe290ec4be8e8b398c19c5499079c [formerly a66a805980f30e2a4e562d16eae509344b4e7911]
    Former-commit-id: 37baa4edb8c308a0af517ae1c6f6ce725d4376a0

--- tests/test_pal.py
+++ tests/test_pal.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """Testing the PAL module"""
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import numpy as np
+import pytest
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, RationalQuadratic
+from sklearn.model_selection import train_test_split
 
-from dispersant_screener.pal import (_get_gp_predictions, _get_uncertainity_region, _union_one_dim)
+from dispersant_screener.pal import (_get_gp_predictions, _get_uncertainity_region, _union_one_dim, pal)
 
 
 def test__get_gp_predictions(make_random_dataset):
@@ -77,7 +79,8 @@ def test__union_one_dim():
     assert (up == np.array([1, 1, 1])).all()
 
 
-def test_pal():
+@pytest.mark.slow
+def test_pal(make_one_dim_test):
     """
     It's a bit tricky to test this one but we can do two things easily.
     (1) If we copy the same target multiple times, we should only find one
@@ -85,4 +88,21 @@ def test_pal():
     (2) We can check how good the hypervolume is.
     """
 
-    ...
+    # CASE 1: One dimension
+
+    X, y = make_one_dim_test  # pylint:disable=invalid-name
+    y = y.reshape(-1, 1)  # pylint:disable=invalid-name
+    gpr = GaussianProcessRegressor(kernel=RationalQuadratic(), n_restarts_optimizer=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.4)  # pylint:disable=invalid-name
+    pareto_optimal, _ = pal([gpr], X_train, y_train, X_test, y_test, hv_reference=[10])
+
+    optimal = np.where(np.array(pareto_optimal) == 1)[0]
+
+    # for one target there should be only one Pareto optimal point
+    assert len(optimal) == 1
+
+    optimum = np.max(y_test)
+    found_optimum = y_test[optimal[0]]
+
+    # we want to be close to the optimum
+    assert np.abs(found_optimum[0] - optimum) < 0.1 * optimum
