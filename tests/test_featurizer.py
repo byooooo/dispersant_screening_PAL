410e8ee8d83598156651204d121708bc3ddcaa1e
Kevin <kevin.jablonka@epfl.ch>
2020-07-29 09:47:14 +0200

    chore: adding tests for featurization
    Former-commit-id: ef828604d7040b26a0e2568ba140eb2d21ee64b7 [formerly 012838d8e9423804e23b9a57eeeffa04af6764b7]
    Former-commit-id: 462f9ac11e8d8d4ee3c035033423cc2fa0025ba0

---
+++ tests/test_featurizer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from dispersant_screener.featurizer import PolymerSmilesFeaturizer
+
+
+def test_head_tail():
+    pmsf = PolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
+    head_tail = PolymerSmilesFeaturizer.get_head_tail_features('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]', pmsf.characters)
+    assert head_tail == {'head_tail_[W]': 2, 'head_tail_[Tr]': 0, 'head_tail_[Ta]': 0, 'head_tail_[R]': 0}
+
+
+def test_clusters():
+    pmsf = PolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
+    cluster_feat = PolymerSmilesFeaturizer.get_cluster_stats(pmsf.smiles, pmsf.replacement_dict)
+    assert cluster_feat['total_clusters'] == 3
+    assert cluster_feat['num_[W]'] == 2 / 3
+    assert cluster_feat['num_[Ta]'] == 1 / 3
+    assert cluster_feat['num_[Tr]'] == 0
+    assert cluster_feat['num_[R]'] == 0
