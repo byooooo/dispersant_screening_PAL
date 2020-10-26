5ac352c863ea065646fe450df76fe49edd83b66f
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-26 09:47:58 +0100

    chore: updating readme

--- tests/test_featurizer.py
+++ tests/test_featurizer.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from dispersant_screener.featurizer import PolymerSmilesFeaturizer
+from dispersant_screener.featurizer import LinearPolymerSmilesFeaturizer
 
 
 def test_head_tail():
-    pmsf = PolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
-    head_tail = PolymerSmilesFeaturizer.get_head_tail_features('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]', pmsf.characters)
+    pmsf = LinearPolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
+    head_tail = LinearPolymerSmilesFeaturizer.get_head_tail_features('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]', pmsf.characters)
     assert head_tail == {'head_tail_[W]': 2, 'head_tail_[Tr]': 0, 'head_tail_[Ta]': 0, 'head_tail_[R]': 0}
 
 
 def test_clusters():
-    pmsf = PolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
-    cluster_feat = PolymerSmilesFeaturizer.get_cluster_stats(pmsf.smiles, pmsf.replacement_dict)
+    pmsf = LinearPolymerSmilesFeaturizer('[W][W][W][W][Ta][Ta][Ta][Ta][W][W][W]')
+    cluster_feat = LinearPolymerSmilesFeaturizer.get_cluster_stats(pmsf.smiles, pmsf.replacement_dict)
     assert cluster_feat['total_clusters'] == 3
     assert cluster_feat['num_[W]'] == 2 / 3
     assert cluster_feat['num_[Ta]'] == 1 / 3
